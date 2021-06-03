97f92d8f8d5971873c5bd49985fac273040be9a8
mahsa-arabi <mahsaarabi26@yahoo.com>
2021-06-03 06:20:32 +0430

    add student side bar

---
+++ studentSideBar.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://v1.fontapi.ir/css/Shabnam" rel="stylesheet">
+</head>
+<body>
+<div id="sideBar">
+    <div id="headerContainer">
+        <p>نام</p>
+        <button>مشاهده اطلاعات</button>
+    </div>
+    <hr/>
+    <ul>
+        <li><a href="#">مشاهده درس های گذرانده</a></li>
+        <li><a href="#">انتخاب واحد</a></li>
+        <li><a href="#">مشاهده کارنامه</a></li>
+    </ul>
+</div>
+</body>
+</html>
