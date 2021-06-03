4bf082db5a6c7728ce22e86fd63349b54b56df20
mahsa-arabi <mahsaarabi26@yahoo.com>
2021-06-03 06:22:05 +0430

    add instructor page

---
+++ instructorPage.php
@@ -0,0 +1,27 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>گلستان</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://v1.fontapi.ir/css/Shabnam" rel="stylesheet">
+</head>
+
+<body>
+<div id="page">
+    <?php
+    require_once ("instructorSideBar.php");
+    ?>
+    <div id="content">
+        <p id="welcome">به سامانه گلستان خوش آمدید</p>
+    </div>
+</div>
+
+
+
+
+</body>
