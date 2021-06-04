b4d5087ff5b85558f4b05c2a18a4d01e1efd00ad
ZGhanbari <z.ghanbari14@gmail.com>
2021-06-04 18:34:41 +0430

    changes

---
+++ src/findCurrentSemester.php
@@ -0,0 +1,14 @@
+<?php
+$semester=null;
+
+if (3<= intval(date("m")) && intval(date("m")) <=5){
+    $semester='Spring';
+}elseif (6<= intval(date("m")) && intval(date("m")) <=8){
+    $semester='Summer';
+}elseif (9<= intval(date("m")) && intval(date("m")) <=11) {
+    $semester = 'Fall';
+}else{
+    $semester=null;
+}
+
+?>
---
+++ src/index.php
@@ -0,0 +1,27 @@
+<!-- 
+
+// require_once 'dbConfig.php';
+// echo "golestan"; -->
+
+
+<?php
+require_once 'dbConfig.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>گلستان</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://v1.fontapi.ir/css/Shabnam" rel="stylesheet">
+</head>
+
+<body>
+<?php
+    require_once("instructorPage.php");
+    ?>
+</body>
+</html> 
