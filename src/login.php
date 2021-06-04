dfb74e2f2b42d17d8d6eb95c2295ffee9e6022f4
ZGhanbari <z.ghanbari14@gmail.com>
2021-06-04 19:25:06 +0430

    add login page

---
+++ src/login.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'dbConfig.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>گلستان</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://v1.fontapi.ir/css/Shabnam" rel="stylesheet">
+</head>
+
+<body>
+<div id="page">
+   
+    <div id="content">
+        
+
+<div id="table">
+<form>
+    <div id="item">
+    <label >ID:</label>
+    <input type="text">
+    </div>
+   <div id="item">
+   <label >Name:</label>
+    <input type="text">
+   </div>
+   <div id="item">
+   <button><a href="studentPage.php">ورود دانشجو</a></button>
+    <button><a href="instructorPage.php">ورود استاد</a></button>
+   </div>
+
+</form>
+</div>
+
+    </div>
+</div>
+
+
+
+
+
+</body>
